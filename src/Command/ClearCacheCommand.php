cbada85282126a2027c62437d17b0778456fe46c
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-03-14 10:14:51 -0400

    Add options to delete specific keys or prefixes from the cache

--- src/Command/ClearCacheCommand.php
+++ src/Command/ClearCacheCommand.php
@@ -4,7 +4,9 @@
 	use DaybreakStudios\PrometheusClient\Adapter\AdapterInterface;
 	use Symfony\Component\Console\Command\Command;
 	use Symfony\Component\Console\Input\InputInterface;
+	use Symfony\Component\Console\Input\InputOption;
 	use Symfony\Component\Console\Output\OutputInterface;
+	use Symfony\Component\Console\Style\SymfonyStyle;
 
 	class ClearCacheCommand extends Command {
 		/**
@@ -24,14 +26,50 @@
 		}
 
 		/**
-		 * @param InputInterface  $input
+		 * {@inheritdoc}
+		 */
+		protected function configure() {
+			$this
+				->addOption('key', 'k', InputOption::VALUE_REQUIRED, 'A key to remove from the cache; may be repeated')
+				->addOption(
+					'prefix',
+					'p',
+					InputOption::VALUE_REQUIRED,
+					'A key prefix to remove the cache; may be repeated'
+				);
+		}
+
+		/**
+		 * @param InputInterface $input
 		 * @param OutputInterface $output
 		 *
 		 * @return int
 		 */
 		protected function execute(InputInterface $input, OutputInterface $output) {
-			if (!$this->adapter->clear())
-				return 1;
+			$io = new SymfonyStyle($input, $output);
+
+			if ($keys = $input->getOption('key')) {
+				foreach ($keys as $key) {
+					if ($this->adapter->delete($key))
+						$io->comment('Deleted ' . $key . ' from cache');
+				}
+			}
+
+			if ($prefixes = $input->getOption('prefix')) {
+				foreach ($prefixes as $prefix) {
+					foreach ($this->adapter->search($prefix) as $key)
+						$this->adapter->delete($key);
+
+					$io->comment('Deleted keys prefixed by ' . $prefix . ' from cache');
+				}
+			}
+
+			if (!$keys && !$prefixes) {
+				if (!$this->adapter->clear())
+					return 1;
+
+				$io->success('Cleared all items from the cache');
+			}
 
 			return 0;
 		}
